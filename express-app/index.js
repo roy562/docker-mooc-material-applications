8981f26433f0d2a72097b12b82a7096adb1b6f72
roy562 <lalit.dream@gmail.com>
2026-01-04 18:09:54 +0530

    GHA Test 5

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1><p>Welcome to the Express.js application - GHA Test 4 with Watchtower.</p>')
+  res.send('<h1>Hello World!</h1><p>Welcome to the Express.js application - GHA Test 5 with Watchtower.</p>')
 })
 
 const PORT = 8080
