f87a5ab350b1185948dc51381ac23c1c28c91945
roy562 <lalit.dream@gmail.com>
2026-01-10 08:51:21 +0530

    GHA Test 8 with EC2

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1><p>Welcome to the Express.js application - GHA Test 7 on EC2.</p>')
+  res.send('<h1>Hello World!</h1><p>Welcome to the Express.js application - GHA Test 8 on EC2.</p>')
 })
 
 const PORT = 8080
